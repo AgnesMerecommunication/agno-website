a749a02164790cb5cde0b3187cb71f940416839c
AgnesMerecommunication <111468187+AgnesMerecommunication@users.noreply.github.com>
2024-04-30 09:53:17 +0000

    update

--- src/components/Data.tsx
+++ src/components/Data.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { BsFacebook, BsWhatsapp, BsEnvelope, BsTelephone, BsLinkedin, BsTwitter, BsTwitterX, BsInstagram, BsSun, BsMoon} from "react-icons/bs";
+import { BsFacebook, BsWhatsapp, BsEnvelope, BsTelephone, BsLinkedin, BsTwitter, BsTwitterX, BsInstagram, BsSun, BsMoon, BsMap} from "react-icons/bs";
 import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Button, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
 import { Tab } from '@headlessui/react'
 import { useEffect, useState } from "react";
@@ -67,120 +67,90 @@ export default function Page({slug} : {slug: string}){
           )
       if(color)
           return(
-              <div style={{backgroundColor : color, color : textColor}}
-              className={`md:px-24 px-2 md:pt-12 pt-0 pb-12`}>
-                  <Navbar isMenuOpen={isMenuOpen} onMenuOpenChange={setIsMenuOpen} 
-                  style={{backgroundColor : color, color : textColor}} maxWidth="full">
-                      <NavbarContent>
-                          <NavbarMenuToggle
-                          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-                          className="sm:hidden"
-                          />
-                          <NavbarBrand>
-                            <p className="font-bold text-inherit  md:text-3xl text-xl">{user?.firstName} {user?.lastName}</p>
-                          </NavbarBrand>
-                      </NavbarContent>
-                      <NavbarContent className="hidden sm:flex gap-4  dark:text-white" justify="end">
-                          <NavbarItem>
-                          <div onClick={()=>openLink("#information")} className=" dark:text-white text-xl cursor-pointer">
-                                  Information
-                          </div>
-                          </NavbarItem>
-                          <NavbarItem > 
-                          <div onClick={()=>openLink("#catalogue")}   className="dark:text-white text-xl  cursor-pointer">
-                                  Catalogue
-                          </div>
-                          </NavbarItem>
-                          <NavbarItem>
-                          <div onClick={()=>openLink("#contact")}
-                          className="bg-[#ff6201] text-gray-200 px-2 py-1 rounded-2xl  flex 
-                                  space-x-2 items-center">
-                              <BsTelephone />
-                                      <span>Contactez</span>
-                          </div>
-                          </NavbarItem>
-                      </NavbarContent>
-                      <NavbarMenu className="dark:bg-white">
-                          <NavbarMenuItem key={''}>
-                              <div  onClick={()=>{openLink("#information"); setIsMenuOpen(!isMenuOpen);}}
-                                    className="font-bold text-xl text-black">
-                                  Informations
-                              </div>
-                          </NavbarMenuItem>
-                          <NavbarMenuItem key={''}>
-                              <div  onClick={()=>{openLink("#catalogue"); setIsMenuOpen(!isMenuOpen);}}
-                                    className="font-bold text-xl text-black">
-                                Catalogue
-                              </div>
-                          </NavbarMenuItem>
-                      </NavbarMenu>
-                  </Navbar>
-                  <div className="flex md:mt-24 mt-12 flex-col md:flex-row" id="information">
-                      <div className="md:w-1/2 flex justify-center">
-                          <img src={user?.picture} alt="" 
-                           className="md:h-96 md:w-96 h-56 w-56 rounded-full md:ml-24" />
+              <div 
+              className={`bg-orange-800	 md:px-24 px-2 md:pt-12 pt-8 pb-12 text-white`}>
+                 <div className="shadow-xl md:px-12 px-2 md:py-20 py-6 bg-gradient-to-tr from-orange-700 via-orange-700 to-orange-400 rounded-xl flex md:flex-row flex-row-reverse">
+                    <div className="md:w-3/5 w-1/2"> 
+                      <div className="md:text-7xl hidden md:flex">AGNO</div>
+                      <div className="`font-bold mb-2 md:mt-12 mt-2 md:text-6xl text-sm">{user?.firstName} {user?.lastName}</div>
+                      <div className="md:text-2xl text-xs">{user?.businessName}</div>
+                      <div className="md:text-xl space-y-2 text-sm ">
+                      <div onClick={()=>openLink("mailto: " + user?.email)}
+                              className=" md:mt-12 md:px-2 py-1 space-x-2 cursor-pointer  text-sm md:text-base flex items-center ">
+                                  <BsEnvelope className=""/> <span>{user?.email}</span>
                       </div>
-                      <div className="md:w-1/2 md:mt-32 mt-12">
-                          <div className="md:text-6xl text-5xl font-bold md:text-left text-center">{user?.firstName} {user?.lastName}</div>
-                          <div className="mt-4  md:text-3xl text-xl">{user?.businessName ? user?.businessName : ''}</div>
-                          <div className="text-white grid gap-2 md:grid-cols-4  grid-cols-1 mt-5 text-xl  pr-5 md:pl-0 pl-5" id="contact">
-                              <div onClick={()=>openLink("mailto: " + user?.email)}
-                              className="bg-[#ff6201]   flex justify-center rounded-full px-2 py-1 items-center space-x-2 cursor-pointer">
-                                  <BsEnvelope/> <span>Email</span>
-                              </div>
-                              {user?.facebook &&  
-                              <div onClick={()=>openLink(user?.facebook ?? '')} className="bg-blue-500 flex
+                      <div onClick={()=>openLink("tel: " + user?.email)}
+                              className="md:px-2 py-1 space-x-2 cursor-pointer text-sm md:text-base flex items-center ">
+                                   <BsTelephone /> <span>{user?.phone}</span>
+                      </div>
+        
+                          {user?.address && <div onClick={()=>openLink("mailto: " + user?.email)}
+                              className="flex md:px-2 py-1 items-center space-x-2 text-sm md:text-base cursor-pointer">
+                                  <BsMap/> <span>{user?.address}</span>
+                              </div>}
+                        
+                      </div>
+                      
+                     <div className="md:mt-12 mt-7">
+                     <a href={carte}   className="border rounded-md p-4 hover:bg-orange-600 text-xs md:text-xl" download>
+                                Telercharger la carte
+                      </a>
+                     </div>
+                    </div>
+
+                    <div className="md:w-2/5 w-1/2 p-2">
+                    <img src={user?.picture} alt="" 
+                           className="md:h-[500px] md:w-96  h-48 w-56 rounded-lg md:mr-24" />
+                    </div>
+                 </div>
+                 
+                 <div className="md:mt-12 mt-5 shadow-xl rounded-xl md:p-12 p-4 bg-gradient-to-tr from-orange-700 via-orange-700 to-orange-400 grid gap-4 md:grid-cols-5 grid-cols-4">
+                 {user?.facebook &&  
+                              <div onClick={()=>openLink(user?.facebook ?? '')} className="bg-orange-600 flex
                               rounded-full px-2 text-center justify-center  py-1 items-center  space-x-2  cursor-pointer">
-                                  <BsFacebook/> <span>Facebook</span>
+                                  <BsFacebook/> <span className="hidden md:flex">Facebook</span>
                               </div>} 
                               {user?.whatsapp && 
                               <div onClick={()=>openLink("https://wa.me/" + user?.whatsapp+ "/?text=Bonjour, j'espère que vous allez bien. J'ai pris votre numéro sur votre site web. Pourrions-nous échanger?")} className="
-                              bg-green-500 flex text-center justify-center  rounded-full px-2 py-1 items-center  space-x-2  cursor-pointer">
-                                      <BsWhatsapp/> <span>Whatsapp</span>
+                              bg-orange-600 flex text-center justify-center  rounded-full px-2 py-1 items-center  space-x-2  cursor-pointer">
+                                      <BsWhatsapp/> <span className="hidden md:flex">Whatsapp</span>
                               </div>  } 
                               {user?.linkedin &&  
-                              <div onClick={()=>openLink(user?.linkedin ?? '')} className="bg-blue-500 flex
+                              <div onClick={()=>openLink(user?.linkedin ?? '')} className="bg-orange-600 flex
                               rounded-full px-2 text-center  justify-center py-1 items-center  space-x-2  cursor-pointer">
-                                  <BsLinkedin/> <span>LinkedIn</span>
+                                  <BsLinkedin/> <span className="hidden md:flex">LinkedIn</span>
                               </div>}   
                               {user?.twitter &&  
-                              <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-blue-500 flex
-                              rounded-full px-2 text-center  py-1 items-center  space-x-2  cursor-pointer">
-                                  <BsTwitterX/> <span>X</span>
+                              <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-orange-600 flex
+                              rounded-full px-2 text-center  py-1 items-center justify-center  space-x-2  cursor-pointer">
+                                  <BsTwitterX/> <span className="hidden md:flex">X(Twitter)</span>
                               </div>}                  
                               {user?.instagram &&  
-                              <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-blue-500 flex
+                              <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-orange-600 flex
                               rounded-full px-2 text-center justify-center  py-1 items-center  space-x-2  cursor-pointer">
-                                  <BsInstagram/> <span>Instagram</span>
-                              </div>}   
-                          </div>
-                      </div>
-                  </div>
-              <div> <div className="flex justify-center font-bold md:mt-24 mt-12 md:text-6xl text-3xl">Carte de visite</div></div>
-              <div  className="flex justify-center md:mt-12 mt-6 md:ml-12 mr-1  pb-12" id="carte">
-                  <img src={carte} alt=""/>
-              </div>
-              <div className="mt-0">
-                  <div> 
-                    <div className="flex justify-center font-bold  md:text-6xl text-3xl">
+                                  <BsInstagram/> <span className="hidden md:flex">Instagram</span>
+                              </div>}  
+                 </div>
+                
+                
+              <div className="md:mt-12 mt-5 md:pt-8 pt-4 bg-gradient-to-tr from-orange-700 via-orange-700 to-orange-400 rounded-xl ">
+              <div className="flex justify-center font-bold  md:text-5xl text-2xl">
                       Catalogue
-                    </div>
-                  </div>   
-              </div>
+                  </div>
               <div className="flex justify-center" id="catalogue"> 
-                  <div className="md:w-[900px] w-full  px-2 py-16 sm:px-0  mr-4 ">
+                  <div className="md:w-[900px] w-full  px-2 md:py-16 py-5 sm:px-0   ">
                       <Tab.Group>
-                          <Tab.List className="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                          <Tab.List className="flex space-x-1 rounded-xl border border-white p-1">
                           
                           {Object.keys(categories).map((category) => (
                               <Tab
                               key={category}
                               className={({ selected }) =>
                                   classNames(
-                                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                                  'w-full rounded-lg py-2.5 text-sm font-medium hover:bg-orange-600',
                                   selected
-                                      ? 'bg-white dark:text-blue-700 text-black'
-                                      : 'dark:text-blue-100 dark:hover:bg-white/[0.12] dark:hover:text-white'
+                                      ? 'bg-orange-600'
+                                      : ''
                                   )
                                 }
                               >
@@ -193,10 +163,10 @@ export default function Page({slug} : {slug: string}){
                           {Object.values(categories).map((posts, idx) => (
                               <Tab.Panel
                               key={idx}
-                              className={'rounded-xl bg-white p-3'}
+                              className={'rounded-xl p-3 bg-blue-900/20'}
                               >
                                 {posts.length == 0 && <div className="flex justify-center  text-black font-bold">Aucun items disponible</div>}
-                              <ul className="grid gap-4 md:grid-cols-4 grid-cols-2">
+                              <ul className="grid gap-4 md:grid-cols-3 grid-cols-2">
                                   {posts.map((post, index) => (
                                   <ProductCard key={index} image={post.picture} title={post.title} description={post.description}/>
                                   ))}
@@ -207,6 +177,8 @@ export default function Page({slug} : {slug: string}){
                       </Tab.Group>
                   </div>
               </div>
+              </div>
+
               </div>
           )
 }
