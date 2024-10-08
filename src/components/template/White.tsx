a10cab42e50628e370f6e0294a88e5ca8b066e57
AgnesMerecommunication <111468187+AgnesMerecommunication@users.noreply.github.com>
2024-10-08 10:02:26 +0000

    Update White.tsx

--- src/components/template/White.tsx
+++ src/components/template/White.tsx
@@ -131,138 +131,102 @@ export default function Page({slug} : {slug: string}){
       
         )
       return(
-        <div 
-        className={`bg-white text-xs md:text-base md:px-24 px-2 md:pt-12 pt-8 pb-12 text-slate-800`}>
-           <div className="shadow-xl md:px-12 px-2 md:py-20 py-6 bg-gradient-to-tr from-slate-200 via-slate-200
-            to-slate-50 rounded-xl flex md:flex-row flex-col-reverse">
-              <div className="md:w-3/5 w-full"> 
-                <div className="`font-bold mb-2 md:mt-12 md:text-7xl text-4xl flex justify-center md:justify-start">
-                  {user?.firstName}
+        <div className={`bg-white text-xs md:text-base md:px-24 px-2 md:pt-12 pt-8 pb-12 text-slate-800`}>
+        <div className="shadow-xl md:px-12 px-2 md:py-20 py-6 bg-gradient-to-tr from-slate-200 via-slate-200 to-slate-50 rounded-xl flex md:flex-row flex-col-reverse">
+            <div className="md:w-3/5 w-full"> 
+                <div className="font-bold mb-2 md:mt-12 text-center md:text-left md:text-7xl text-4xl">
+                    {user?.firstName}
                 </div>
-                <div className="md:text-2xl text-xs flex justify-center md:justify-start">{user?.businessName}</div>
+                <div className="md:text-2xl text-xs text-center md:text-left">{user?.businessName}</div>
                 <div className="md:text-xl space-y-2 text-sm ">
-                  <div onClick={()=>openLink("mailto: " + user?.email)}
-                          className=" md:mt-12 md:px-2 py-1 space-x-2 cursor-pointer 
-                            text-xs md:text-base flex items-center  justify-center md:justify-start">
-                              <BsEnvelope className=""/> 
-                              <span className="break-words whitespace-normal">{user?.email}</span>
-                  </div>
-                  <div onClick={()=>openLink("tel: " + user?.email)}
-                        className="md:px-2 py-1 space-x-2 cursor-pointer text-xs md:text-base flex items-center  justify-center md:justify-start">
-                             <BsTelephone /> <span className="truncate text-wrap">{user?.phone}</span>
-                  </div>
-                    {user?.address && <div onClick={()=>openLink("mailto: " + user?.email)}
-                        className="flex md:px-2 py-1 items-center space-x-2 text-xs md:text-base cursor-pointer  justify-center md:justify-start">
-                            <BsMap/> <span className="truncate text-wrap">{user?.address}</span>
-                        </div>}
+                    <div onClick={() => openLink("mailto:" + user?.email)}
+                         className="md:mt-12 md:px-2 py-1 space-x-2 cursor-pointer text-xs md:text-base flex items-center justify-center md:justify-start">
+                        <BsEnvelope /> 
+                        <span className="break-words whitespace-normal">{user?.email}</span>
+                    </div>
+                    <div onClick={() => openLink("tel:" + user?.phone)}
+                         className="md:px-2 py-1 space-x-2 cursor-pointer text-xs md:text-base flex items-center justify-center md:justify-start">
+                        <BsTelephone /> 
+                        <span className="truncate text-wrap">{user?.phone}</span>
+                    </div>
+                    {user?.address && 
+                        <div onClick={() => openLink("mailto:" + user?.email)}
+                             className="flex md:px-2 py-1 items-center space-x-2 text-xs md:text-base cursor-pointer justify-center md:justify-start">
+                            <BsMap /> <span className="truncate text-wrap">{user?.address}</span>
+                        </div>
+                    }
                 </div>
-                <div onClick={()=>openVcf()}  className="border md:mt-12 mt-7 text-center rounded-md md:p-4 p-2 bg-slate-300  text-xs md:text-xl md:w-1/2 cursor-pointer">
-                           Telecharger mon contact
+                <div onClick={() => openVcf()} className="border md:mt-12 mt-7 text-center rounded-md md:p-4 p-2 bg-slate-300 text-xs md:text-xl md:w-1/2 cursor-pointer">
+                    Telecharger mon contact
                 </div>
-                <div onClick={()=>openCarte()}   className="border md:mt-4 mt-2 text-center rounded-md md:p-4 p-2 bg-slate-300 text-xs md:text-xl md:w-1/2 cursor-pointer">
-                          Telercharger la carte
+                <div onClick={() => openCarte()} className="border md:mt-4 mt-2 text-center rounded-md md:p-4 p-2 bg-slate-300 text-xs md:text-xl md:w-1/2 cursor-pointer">
+                    Telecharger la carte
                 </div>
-              </div>
-              <div className="md:w-2/5 w-full p-2 flex flex-col justify-center items-center">
-              {/*imageUrl &&<img src={imageUrl} alt=""  
-                     className="md:h-96 md:w-96  h-56 w-56 rounded-full md:hidden" />*/}
-                     <div className="md:h-96 md:w-96  h-56 w-56">
-                     <Image src={imageUrl}  alt={"Image de profil"} height={384} width={384} 
-              className="rounded-full" sizes="(max-width: 768px) 100vw"/>
-                     </div>
-             
-               {/*<Image src={imageUrl} alt={"Image de profil"} height={384} width={384} className="rounded-full hidden md:flex"/>
-                 imageUrl && <img src={imageUrl} alt=""  
-                     className="md:h-96 md:w-96  h-56 w-56 " />*/}
-              </div>
-           </div>
-           
-           <div className="md:mt-12 mt-5 shadow-xl rounded-xl md:p-12 p-4 bg-gradient-to-tr from-slate-200 via-slate-200 to-slate-50 grid gap-4 md:grid-cols-5 grid-cols-4">
-           <div onClick={()=>openLink("sms:" + user?.phone)} className="
-                        bg-slate-300 flex text-center justify-center  rounded-full px-2 py-1 items-center  space-x-2  cursor-pointer">
-                                <BiMessage className="text-yellow-500"/> <span className="hidden md:flex">SMS</span>
-                        </div> 
+            </div>
+            <div className="md:w-2/5 w-full p-2 flex flex-col justify-center items-center">
+                <div className="md:h-96 md:w-96 h-56 w-56">
+                    <Image src={imageUrl} alt="Image de profil" height={384} width={384} className="rounded-full" sizes="(max-width: 768px) 100vw"/>
+                </div>
+            </div>
+        </div>
+    
+        <div className="md:mt-12 mt-5 shadow-xl rounded-xl md:p-12 p-4 bg-gradient-to-tr from-slate-200 via-slate-200 to-slate-50 grid gap-4 md:grid-cols-5 grid-cols-4">
+            <div onClick={() => openLink("sms:" + user?.phone)} className="bg-slate-300 flex text-center justify-center rounded-full px-2 py-1 items-center space-x-2 cursor-pointer">
+                <BiMessage className="text-yellow-500"/> <span className="hidden md:flex">SMS</span>
+            </div> 
             {user?.facebook &&  
-                        <div onClick={()=>openLink(user?.facebook ?? '')} className="bg-slate-300 flex
-                        rounded-full px-2 text-center justify-center  py-1 items-center  space-x-2  cursor-pointer">
-                            <BsFacebook className="text-blue-700"/> <span className="hidden md:flex">Facebook</span>
-                        </div>} 
-                        {user?.whatsapp && 
-                        <div onClick={()=>openLink("https://wa.me/" + user?.whatsapp+ "/?text=Bonjour, j'espère que vous allez bien. J'ai pris votre numéro sur votre site web. Pourrions-nous échanger?")} className="
-                        bg-slate-300 flex text-center justify-center  rounded-full px-2 py-1 items-center  space-x-2  cursor-pointer">
-                                <BsWhatsapp className="text-green-700"/> <span className="hidden md:flex">Whatsapp</span>
-                        </div>  } 
-                        {user?.linkedin &&  
-                        <div onClick={()=>openLink(user?.linkedin ?? '')} className="bg-slate-300 flex
-                        rounded-full px-2 text-center  justify-center py-1 items-center  space-x-2  cursor-pointer">
-                            <BsLinkedin/> <span className="hidden md:flex">LinkedIn</span>
-                        </div>}   
-                        {user?.twitter &&  
-                        <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-slate-300 flex
-                        rounded-full px-2 text-center  py-1 items-center justify-center  space-x-2  cursor-pointer">
-                            <BsTwitterX/> <span className="hidden md:flex">X(Twitter)</span>
-                        </div>}                  
-                        {user?.instagram &&  
-                        <div onClick={()=>openLink(user?.twitter ?? '')} className="bg-slate-300 flex
-                        rounded-full px-2 text-center justify-center  py-1 items-center  space-x-2  cursor-pointer">
-                            <BsInstagram className="text-fuchsia-700"/> <span className="hidden md:flex">Instagram</span>
-                        </div>}  
-           </div>
-          
-       <div>
-          <div className="md:mt-12 mt-5 md:pt-8 pt-4 bg-gradient-to-tr from-slate-200 via-slate-200 to-slate-50 rounded-xl ">
-          <div className="flex justify-center font-bold  md:text-5xl text-2xl">
-                  Catalogue
-              </div>
-          <div className="flex justify-center" id="catalogue"> 
-              <div className="md:w-[900px] w-full  px-2 md:py-16 py-5 sm:px-0   ">
-                  <Tab.Group>
-                      <Tab.List className="flex space-x-1 rounded-xl border border-black p-1">
-                      
-                      {Object.keys(categories).map((category) => (
-                          <Tab
-                          key={category}
-                          className={({ selected }) =>
-                              classNames(
-                              'w-full rounded-lg py-2.5 text-sm font-medium hover:bg-slate-300',
-                              selected
-                                  ? 'bg-slate-300'
-                                  : ''
-                              )
-                            }
-                          >
-                          {category}
-                          </Tab>
-                      ))}
-                      </Tab.List>
-                      <Tab.Panels className="mt-2">
-                        
-                      {Object.values(categories).map((posts, idx) => (
-                          <Tab.Panel
-                          key={idx}
-                          className={'rounded-xl p-3 bg-slate-300'}
-                          >
-                            {posts.length == 0 && <div className="flex justify-center  text-black font-bold">Aucun items disponible</div>}
-                          <ul className="grid gap-4 md:grid-cols-3 grid-cols-1">
-                              {posts.map((post, index) => (
-                                post.title == "CATALOG" ?<CatalogueCard 
-                                whatsapp={user?.whatsapp} title={post.title} email={user?.email} image={post.picture}/> :
-                              <ProductCard key={index} whatsapp={user?.whatsapp} 
-                              image={post.picture} title={post.title} 
-                              description={post.description} email={user?.email}/>
-                              ))}
-                          </ul>
-                          </Tab.Panel>
-                      ))}
-                      </Tab.Panels>
-                  </Tab.Group>
-              </div>
-          </div>
-          </div>
+                <div onClick={() => openLink(user?.facebook ?? '')} className="bg-slate-300 flex rounded-full px-2 text-center justify-center py-1 items-center space-x-2 cursor-pointer">
+                    <BsFacebook className="text-blue-700"/> <span className="hidden md:flex">Facebook</span>
+                </div>} 
+            {user?.whatsapp && 
+                <div onClick={() => openLink("https://wa.me/" + user?.whatsapp + "/?text=Bonjour, j'espère que vous allez bien.")} className="bg-slate-300 flex text-center justify-center rounded-full px-2 py-1 items-center space-x-2 cursor-pointer">
+                    <BsWhatsapp className="text-green-700"/> <span className="hidden md:flex">Whatsapp</span>
+                </div>} 
+            {user?.linkedin &&  
+                <div onClick={() => openLink(user?.linkedin ?? '')} className="bg-slate-300 flex rounded-full px-2 text-center justify-center py-1 items-center space-x-2 cursor-pointer">
+                    <BsLinkedin/> <span className="hidden md:flex">LinkedIn</span>
+                </div>}   
+            {user?.twitter &&  
+                <div onClick={() => openLink(user?.twitter ?? '')} className="bg-slate-300 flex rounded-full px-2 text-center py-1 items-center justify-center space-x-2 cursor-pointer">
+                    <BsTwitterX/> <span className="hidden md:flex">X(Twitter)</span>
+                </div>}                  
+            {user?.instagram &&  
+                <div onClick={() => openLink(user?.instagram ?? '')} className="bg-slate-300 flex rounded-full px-2 text-center justify-center py-1 items-center space-x-2 cursor-pointer">
+                    <BsInstagram className="text-fuchsia-700"/> <span className="hidden md:flex">Instagram</span>
+                </div>}  
         </div>
-        
-
+    
+        <div className="md:mt-12 mt-5 md:pt-8 pt-4 bg-gradient-to-tr from-slate-200 via-slate-200 to-slate-50 rounded-xl">
+            <div className="flex justify-center font-bold md:text-5xl text-2xl">Catalogue</div>
+            <div className="flex justify-center" id="catalogue"> 
+                <div className="md:w-[900px] w-full px-2 md:py-16 py-5 sm:px-0">
+                    <Tab.Group>
+                        <Tab.List className="flex space-x-1 rounded-xl border border-black p-1">
+                            {Object.keys(categories).map((category) => (
+                                <Tab key={category} className={({ selected }) => classNames('w-full rounded-lg py-2.5 text-sm font-medium hover:bg-slate-300', selected ? 'bg-slate-300' : '')}>
+                                    {category}
+                                </Tab>
+                            ))}
+                        </Tab.List>
+                        <Tab.Panels className="mt-2">
+                            {Object.values(categories).map((posts, idx) => (
+                                <Tab.Panel key={idx} className={'rounded-xl p-3 bg-slate-300'}>
+                                    {posts.length === 0 && <div className="flex justify-center text-black font-bold">Aucun items disponible</div>}
+                                    <ul className="grid gap-4 md:grid-cols-3 grid-cols-1">
+                                        {posts.map((post, index) => (
+                                            post.title === "CATALOG" ? <CatalogueCard whatsapp={user?.whatsapp} title={post.title} email={user?.email} image={post.picture} /> :
+                                            <ProductCard key={index} whatsapp={user?.whatsapp} image={post.picture} title={post.title} description={post.description} email={user?.email} />
+                                        ))}
+                                    </ul>
+                                </Tab.Panel>
+                            ))}
+                        </Tab.Panels>
+                    </Tab.Group>
+                </div>
+            </div>
         </div>
+    </div>
+    
     )
          
 }
